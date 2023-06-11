d99c8e82b628cdef07ae71f14ee1f01addb2c4ce
Woozyzzz <woozyzzz@foxmail.com>
2023-06-12 02:14:10 +0800

    feat(index): 新增模拟查询接口数据及问答卡片展开收起交互

--- src/main.js
+++ src/main.js
@@ -1,1 +1,196 @@
-console.log("hi");
+const el = (selector) => document.querySelector(selector);
+
+// 元素
+const elFormNewChatButton = el(`.form__new-chat-button`);
+const elAsideList = el(`.aside__list`);
+const elMainArticle = el(`.main__article`);
+const elQuestionTextarea = el(`.question__textarea`);
+const elQuestionSubmitButton = el(`.question__submit-button`);
+
+// 监听器
+elFormNewChatButton.addEventListener("click", () => {
+  elMainArticle.innerHTML = "";
+  elQuestionTextarea.focus();
+});
+elAsideList.addEventListener("click", async (event) => {
+  const {
+    target: { className },
+  } = event;
+  if (className !== "list-item") {
+    return;
+  }
+  elMainArticle.innerHTML = "";
+  await fetchChatHistoryList().catch((error) => {
+    throw error;
+  });
+});
+elMainArticle.addEventListener("click", (event) => {
+  const {
+    target: { className },
+    target,
+  } = event;
+  if (!className.includes("content__more-button")) {
+    return;
+  }
+  if (className.includes("expanded")) {
+    target.innerHTML = "展开更多";
+    target.classList.remove("expanded");
+    target.previousSibling.classList.remove("expanded");
+  } else {
+    target.innerHTML = "收起更多";
+    target.classList.add("expanded");
+    target.previousSibling.classList.add("expanded");
+  }
+});
+elQuestionSubmitButton.addEventListener("click", () => {
+  console.log(elQuestionTextarea.value);
+});
+
+// 数据
+let currentTitleHistoryList = [];
+let currentChatHistoryList = [];
+
+// 模拟接口定义
+const fetchTitleHistoryList = async () => {
+  const { data } = await fetch("public/fetch-title-history-list.json")
+    .then((response) => response.json())
+    .catch((error) => {
+      throw error;
+    });
+  currentTitleHistoryList = Array.isArray(data) ? data : [];
+  renderCurrentTitleHistoryList();
+};
+const fetchChatHistoryList = async () => {
+  const { data } = await fetch(
+    `public/fetch-chat-history-list-${randomNumber(3)}.json`
+  )
+    .then((response) => response.json())
+    .catch((error) => {
+      throw error;
+    });
+  currentChatHistoryList = Array.isArray(data) ? data : [];
+  renderCurrentChatHistoryList();
+};
+
+// 数据处理方法
+const randomNumber = (num) => {
+  return Math.floor(Math.random() * num);
+};
+// 创建函数来递归生成 HTML 结构
+const createTreeNode = (node) => {
+  const element = document.createElement(node.tag);
+  if (node.classList) {
+    for (const className of node.classList) {
+      element.classList.add(className);
+    }
+  }
+  if (node.text) {
+    element.textContent = node.text;
+  }
+  if (node.children) {
+    for (const childNode of node.children) {
+      const childElement = createTreeNode(childNode);
+      element.appendChild(childElement);
+    }
+  }
+  return element;
+};
+
+// 渲染方法
+const renderCurrentTitleHistoryList = () => {
+  const treeData =
+    currentTitleHistoryList.map((currentTitleHistoryItem) => {
+      const { content } = currentTitleHistoryItem;
+      const titleValue = {
+        tag: "li",
+        classList: ["list-item"],
+        text: content,
+      };
+      return titleValue;
+    }) || [];
+  for (const node of treeData) {
+    const element = createTreeNode(node);
+    elAsideList.appendChild(element);
+  }
+};
+const renderCurrentChatHistoryList = () => {
+  const treeData =
+    currentChatHistoryList.map((currentChatHistoryItem) => {
+      const { role, content } = currentChatHistoryItem;
+      const userValue = {
+        tag: "section",
+        classList: ["article__section", "user"],
+        children: [
+          { tag: "div", classList: ["section__profile"] },
+          {
+            tag: "div",
+            classList: ["section__content"],
+            text: content,
+          },
+        ],
+      };
+      const thresholdContentLength = 500;
+      const chatValue =
+        content.length > thresholdContentLength
+          ? {
+              tag: "section",
+              classList: ["article__section"],
+              children: [
+                { tag: "div", classList: ["section__profile"] },
+                {
+                  tag: "div",
+                  classList: ["section__content"],
+                  children: [
+                    {
+                      tag: "div",
+                      classList: ["content__text", "more"],
+                      text: content,
+                    },
+                    {
+                      tag: "span",
+                      classList: ["content__more-button"],
+                      text: "展开更多",
+                    },
+                  ],
+                },
+              ],
+            }
+          : {
+              tag: "section",
+              classList: ["article__section"],
+              children: [
+                { tag: "div", classList: ["section__profile"] },
+                {
+                  tag: "div",
+                  classList: ["section__content"],
+                  children: [
+                    {
+                      tag: "div",
+                      classList: ["content__text"],
+                      text: content,
+                    },
+                  ],
+                },
+              ],
+            };
+      const hashMap = new Map().set(1, userValue).set(2, chatValue);
+      return hashMap.get(role);
+    }) || [];
+
+  for (const node of treeData) {
+    const element = createTreeNode(node);
+    elMainArticle.appendChild(element);
+  }
+};
+
+// 主函数
+const main = async () => {
+  await fetchTitleHistoryList().catch((error) => {
+    throw error;
+  });
+  await fetchChatHistoryList().catch((error) => {
+    throw error;
+  });
+};
+
+main();
